9a220c44129a3e111076417820e023e8db07f046
Denis Erygin <denisio.bro@gmail.com>
2013-10-28 16:34:42 +0400

    initial commit

---
+++ database.sql
@@ -0,0 +1,105 @@
+--
+-- Table structure for table `Character_DATA`
+--
+
+DROP TABLE IF EXISTS `Character_DATA`;
+CREATE TABLE `Character_DATA` (
+  `CharacterID` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `PlayerUID` varchar(32) NOT NULL,
+  `InstanceID` int(11) NOT NULL,
+  `Datestamp` datetime DEFAULT NULL,
+  `LastLogin` datetime NOT NULL,
+  `Inventory` longtext,
+  `Backpack` longtext,
+  `Worldspace` varchar(70) NOT NULL DEFAULT '[]',
+  `Medical` varchar(256) NOT NULL DEFAULT '[]',
+  `Alive` tinyint(4) unsigned NOT NULL DEFAULT '1',
+  `Generation` int(11) unsigned NOT NULL DEFAULT '1',
+  `LastAte` datetime NOT NULL,
+  `LastDrank` datetime NOT NULL,
+  `KillsZ` int(11) unsigned NOT NULL DEFAULT '0',
+  `HeadshotsZ` int(11) unsigned NOT NULL DEFAULT '0',
+  `DistanceFoot` int(11) NOT NULL DEFAULT '0',
+  `Duration` int(11) unsigned NOT NULL DEFAULT '0',
+  `CurrentState` varchar(128) NOT NULL DEFAULT '[]',
+  `KillsH` int(11) unsigned NOT NULL DEFAULT '0',
+  `Model` varchar(50) NOT NULL DEFAULT '"Survivor2_DZ"',
+  `KillsB` int(11) unsigned NOT NULL DEFAULT '0',
+  `Humanity` int(11) NOT NULL DEFAULT '2500',
+  PRIMARY KEY (`CharacterID`),
+  KEY `CharFetch` (`PlayerUID`,`Alive`) USING BTREE
+) ENGINE=MyISAM AUTO_INCREMENT=25998 DEFAULT CHARSET=utf8;
+
+--
+-- Table structure for table `Object_DATA`
+--
+
+DROP TABLE IF EXISTS `Object_DATA`;
+CREATE TABLE `Object_DATA` (
+  `ObjectID` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `ObjectUID` bigint(20) NOT NULL DEFAULT '0',
+  `Instance` int(11) NOT NULL,
+  `Classname` varchar(50) DEFAULT NULL,
+  `Datestamp` datetime NOT NULL,
+  `CharacterID` int(11) unsigned NOT NULL DEFAULT '0',
+  `Worldspace` varchar(70) NOT NULL DEFAULT '[]',
+  `Inventory` longtext,
+  `Hitpoints` varchar(512) NOT NULL DEFAULT '[]',
+  `Fuel` double(13,5) NOT NULL DEFAULT '1.00000',
+  `Damage` double(13,5) NOT NULL DEFAULT '0.00000',
+  `last_updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`ObjectID`),
+  KEY `ObjectUID` (`ObjectUID`),
+  KEY `Instance` (`Instance`)
+) ENGINE=MyISAM AUTO_INCREMENT=843 DEFAULT CHARSET=utf8;
+
+--
+-- Table structure for table `Object_init_DATA`
+--
+
+DROP TABLE IF EXISTS `Object_init_DATA`;
+CREATE TABLE `Object_init_DATA` (
+  `ObjectID` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `ObjectUID` bigint(20) NOT NULL DEFAULT '0',
+  `Instance` int(11) NOT NULL,
+  `Classname` varchar(50) DEFAULT NULL,
+  `Datestamp` datetime NOT NULL,
+  `CharacterID` int(11) unsigned NOT NULL DEFAULT '0',
+  `Worldspace` varchar(70) NOT NULL DEFAULT '[]',
+  `Inventory` longtext,
+  `Hitpoints` varchar(512) NOT NULL DEFAULT '[]',
+  `Fuel` double(13,5) NOT NULL DEFAULT '1.00000',
+  `Damage` double(13,5) NOT NULL DEFAULT '0.00000',
+  `last_updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`ObjectID`),
+  KEY `ObjectUID` (`ObjectUID`),
+  KEY `Instance` (`Instance`)
+) ENGINE=MyISAM AUTO_INCREMENT=90 DEFAULT CHARSET=utf8;
+
+--
+-- Table structure for table `Player_DATA`
+--
+
+DROP TABLE IF EXISTS `Player_DATA`;
+CREATE TABLE `Player_DATA` (
+  `PlayerUID` varchar(32) NOT NULL,
+  `PlayerName` varchar(128) NOT NULL,
+  `PlayerMorality` int(11) NOT NULL DEFAULT '0',
+  `PlayerSex` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`PlayerUID`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Table structure for table `Player_LOGIN`
+--
+
+DROP TABLE IF EXISTS `Player_LOGIN`;
+CREATE TABLE `Player_LOGIN` (
+  `LoginID` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `PlayerUID` varchar(32) NOT NULL,
+  `CharacterID` int(11) unsigned NOT NULL,
+  `Datestamp` datetime NOT NULL,
+  `Action` tinyint(3) NOT NULL,
+  PRIMARY KEY (`LoginID`)
+) ENGINE=MyISAM AUTO_INCREMENT=50929 DEFAULT CHARSET=utf8;
+
